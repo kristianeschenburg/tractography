ca7f5e4dd99d3d635e6d86036300e0a8aa978509
Kristian Eschenburg <keschenb@uw.edu>
2018-11-16 15:33:13 -0800

    w2w

--- white_fibers.sh
+++ white_fibers.sh
@@ -3,7 +3,7 @@
 subject=$1
 samples=$2
 
-PTX3=/mnt/parcellator/parcellation/Software/fslbuild/fsl/src/ptx3/probtrackx3
+PTX2=/mnt/parcellator/parcellation/Software/fslbuild/fsl/src/ptx3/probtrackx3
 hcp_dir=/mnt/parcellator/parcellation/HCP/Connectome_4
 data_dir=/mnt/parcellator/parcellation/parcellearning/Data
 conn_dir=/Connectome_4
@@ -16,4 +16,4 @@ ${PTX3} -s ${hcp_dir}/${subject}/Diffusion.bedpostX/merged \
         --seedref=${conn_dir}/${subject}/T1w/T1w_acpc_dc_restore.nii.gz \
         --omatrix1 \
         --forcefirststep \
-        -V 1 -P ${samples} --opd --ompl --onewayonly --savepoints
+        -V 1 -P ${samples} --opd --ompl --onewayonly
