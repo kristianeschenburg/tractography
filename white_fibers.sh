99a428186fae36d6fb20dc756899ba2389fee453
Kristian Eschenburg <keschenb@uw.edu>
2018-11-16 01:38:24 -0800

    white 2 white tractography

---
+++ white_fibers.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+subject=$1
+samples=$2
+
+PTX3=/mnt/parcellator/parcellation/Software/fslbuild/fsl/src/ptx3/probtrackx3
+hcp_dir=/mnt/parcellator/parcellation/HCP/Connectome_4
+data_dir=/mnt/parcellator/parcellation/parcellearning/Data
+conn_dir=/Connectome_4
+
+${PTX3} -s ${hcp_dir}/${subject}/Diffusion.bedpostX/merged \
+        -m ${hcp_dir}/${subject}/Diffusion/nodif_brain_mask \
+        -x ${conn_dir}/${subject}/T1w/fsaverage_LR32k/${subject}.L.white.32k_fs_LR.surf.gii \
+        --stop=${conn_dir}/${subject}/T1w/fsaverage_LR32k/${subject}.L.white.32k_fs_LR.surf.gii \
+        --dir=${data_dir}/WhiteFibers/${subject}.L.W2W --forcedir \
+        --seedref=${conn_dir}/${subject}/T1w/T1w_acpc_dc_restore.nii.gz \
+        --omatrix1 \
+        --forcefirststep \
+        -V 1 -P ${samples} --opd --ompl --onewayonly --savepoints
