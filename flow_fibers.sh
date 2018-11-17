a2f74a72a3032acd7b394790c3a61f88eb8daf87
Kristian Eschenburg <keschenb@uw.edu>
2018-11-16 20:29:10 -0800

    fiber streamlines

--- flow_fibers.sh
+++ flow_fibers.sh
@@ -8,14 +8,13 @@ samples=$4
 PTX3=/mnt/parcellator/parcellation/Software/fslbuild/fsl/src/ptx3/probtrackx3
 hcp_dir=/mnt/parcellator/parcellation/HCP/Connectome_4
 data_dir=/mnt/parcellator/parcellation/parcellearning/Data
-conn_dir=/Connectome_4
 
 ${PTX3} -s ${hcp_dir}/${subject}/Diffusion.bedpostX/merged \
         -m ${hcp_dir}/${subject}/Diffusion/nodif_brain_mask \
         -x ${data_dir}/FlowLines/${subject}.L.white.Flow.${upper}.nb.10.diff.5.acpc.surf.gii \
         --stop=${conn_dir}/${subject}/T1w/fsaverage_LR32k/${subject}.L.white.32k_fs_LR.surf.gii \
         --dir=${data_dir}/FlowFibers/${subject}.L.${upper}.${lower} --forcedir \
-        --omatrix3 \
+        --omatrix3 --disttresh3=${lenthresh} \
         --target3=${data_dir}/FlowLines/${subject}.L.white.Flow.${upper}.nb.10.diff.5.acpc.surf.gii \
         --lrtarget3=${conn_dir}/${subject}/T1w/fsaverage_LR32k/${subject}.L.white.32k_fs_LR.surf.gii \
         --seedref=${conn_dir}/${subject}/T1w/T1w_acpc_dc_restore.nii.gz \
--- white_fibers.sh
+++ white_fibers.sh
@@ -2,18 +2,18 @@
 
 subject=$1
 samples=$2
+lenthresh=$3
 
-PTX2=/mnt/parcellator/parcellation/Software/fslbuild/fsl/src/ptx3/probtrackx3
+PTX3=/mnt/parcellator/parcellation/Software/fslbuild/fsl/src/ptx3/probtrackx3
 hcp_dir=/mnt/parcellator/parcellation/HCP/Connectome_4
 data_dir=/mnt/parcellator/parcellation/parcellearning/Data
-conn_dir=/Connectome_4
 
 ${PTX3} -s ${hcp_dir}/${subject}/Diffusion.bedpostX/merged \
         -m ${hcp_dir}/${subject}/Diffusion/nodif_brain_mask \
-        -x ${conn_dir}/${subject}/T1w/fsaverage_LR32k/${subject}.L.white.32k_fs_LR.surf.gii \
-        --stop=${conn_dir}/${subject}/T1w/fsaverage_LR32k/${subject}.L.white.32k_fs_LR.surf.gii \
-        --dir=${data_dir}/WhiteFibers/${subject}.L.W2W --forcedir \
-        --seedref=${conn_dir}/${subject}/T1w/T1w_acpc_dc_restore.nii.gz \
+        -x ${data_dir}/FlowSurfaces/${subject}.L.white.acpc.gii \
+        --stop=${data_dir}/Surfaces/${subject}.L.white.32k_fs_LR.acpc_dc.surf.gii \
+        --dir=${data_dir}/WhiteFibers/${subject}.L.W2W.Len.${lenthresh} --forcedir \
+        --seedref=${data_dir}/Structural/${p}.T1w_acpc_dc_restore.nii.gz \
         --omatrix1 \
         --forcefirststep \
-        -V 1 -P ${samples} --opd --ompl --onewayonly
+        -V 1 -P ${samples} --opd --ompl --onewayonly --distthresh1=${lenthresh}
