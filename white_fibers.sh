72d543bed6f05384ff774b585775f303be91c24d
Kristian Eschenburg <keschenb@uw.edu>
2018-11-17 17:50:17 -0800

    update white

--- white_fibers.sh
+++ white_fibers.sh
@@ -10,12 +10,12 @@ data_dir=/mnt/parcellator/parcellation/parcellearning/Data
 
 ${PTX3} -s ${hcp_dir}/${subject}/Diffusion.bedpostX/merged \
         -m ${hcp_dir}/${subject}/Diffusion/nodif_brain_mask \
-        -x ${data_dir}/FlowSurfaces/${subject}.L.white.acpc.gii \
+        -x ${data_dir}/FlowSurfaces/${subject}.L.WholeIPL.white.acpc.gii \
         --stop=${data_dir}/Surfaces/${subject}.L.white.32k_fs_LR.acpc_dc.surf.gii \
         --dir=${data_dir}/WhiteFibers/${subject}.L.W2W.Len.${lenthresh} --forcedir \
         --seedref=${data_dir}/Structural/${subject}.T1w_acpc_dc_restore.nii.gz \
         --omatrix3 \
-        --target3=${data_dir}/FlowSurfaces/${subject}.L.white.acpc.gii \
+        --target3=${data_dir}/FlowSurfaces/${subject}.L.WholeIPL.white.acpc.gii \
         --lrtarget3=${data_dir}/Surfaces/${subject}.L.white.32k_fs_LR.acpc_dc.surf.gii \
         --forcefirststep \
         -V 1 -P ${samples} --opd --ompl --onewayonly --distthresh1=${lenthresh}
