1b202cb22538083506768285f5b40be5ab1b3bf0
Kristian Eschenburg <keschenb@uw.edu>
2018-11-17 17:48:07 -0800

    update white

--- white_fibers.sh
+++ white_fibers.sh
@@ -13,7 +13,7 @@ ${PTX3} -s ${hcp_dir}/${subject}/Diffusion.bedpostX/merged \
         -x ${data_dir}/FlowSurfaces/${subject}.L.white.acpc.gii \
         --stop=${data_dir}/Surfaces/${subject}.L.white.32k_fs_LR.acpc_dc.surf.gii \
         --dir=${data_dir}/WhiteFibers/${subject}.L.W2W.Len.${lenthresh} --forcedir \
-        --seedref=${data_dir}/Structural/${p}.T1w_acpc_dc_restore.nii.gz \
+        --seedref=${data_dir}/Structural/${subject}.T1w_acpc_dc_restore.nii.gz \
         --omatrix3 \
         --target3=${data_dir}/FlowSurfaces/${subject}.L.white.acpc.gii
         --lrtarget3=${data_dir}/Surfaces/${subject}.L.white.32k_fs_LR.acpc_dc.surf.gii
